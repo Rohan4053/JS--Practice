6e75f2284d458efe8f4c419daf9097f9b1cf414a
Rohan4053 <144653383+Rohan4053@users.noreply.github.com>
2025-02-06 10:20:08 +0000

    Config

--- .devcontainer/01_Basics/03_conversions.js
+++ .devcontainer/01_Basics/03_conversions.js
@@ -1,30 +1,52 @@
-let score="true" //On printing score it gives 0
+// let score="true" //On printing score it gives 0
 
-console.log(typeof score)    // number
-console.log(typeof(score))   // number
+// console.log(typeof score)    // number
+// console.log(typeof(score))   // number
 
 
 
-let valueInNumber = Number(score)  /// converted to number
-console.log(typeof valueInNumber);  //number
-console.log(valueInNumber);  // NaN
-console.log(score) 
-console.log(typeof score) 
+// let valueInNumber = Number(score)  /// converted to number
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber);  // NaN
+// console.log(score) 
+// console.log(typeof score) 
 
-//"33"=> 33
-// "33abc"=> NaN
-// true=> 1; false=> 0
+// //"33"=> 33
+// // "33abc"=> NaN
+// // true=> 1; false=> 0
 
-let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);  //true
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);  //true
 
-//1=> true
-// " " => false
-// "hitesh" => true
+// //1=> true
+// // " " => false
+// // "hitesh" => true
 
-let someNumber=22
+// let someNumber=22
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-console.log(typeof someNumber)
+// console.log(typeof someNumber)
+
+
+
+// // ****************Operations*************************
+
+// let value=3
+// let neg=-value
+// console.log(neg)
+
+console.log(2**3)
+let saw="h"
+let high="ki"
+console.log(high+saw)
+
+console.log("10"+2)        // 102
+console.log("10"+2+4)      // 1024
+console.log(7+8+"10"+2+4)  // 151024          Resource-> tc39.es
+console.log((7+8)*9%10)
+
+
+console.log(+true)
+console.log(+" ")
