f53e6e0843212af63476adbabde0270597ced0de
Rohan4053 <144653383+Rohan4053@users.noreply.github.com>
2025-02-07 21:24:18 +0000

    DONE

---
+++ .devcontainer/01_Basics/06_Strings.js
@@ -0,0 +1,16 @@
+const name="Ring"
+const repocount=45
+
+//console.log(name+repocount);
+console.log(`Hello my name is ${name} and this is ${repocount}`);
+
+const game= new String("MYDEAR")
+
+console.log(game[0]);
+console.log(game.__proto__);
+console.log(game.indexOf('D'))
+
+const anotherString = game.slice(-8,4)
+console.log(anotherString);
+
+
