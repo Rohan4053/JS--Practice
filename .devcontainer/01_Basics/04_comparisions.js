6506d3f5469ccab438bead5a3d4e22e32ae628bc
Rohan4053 <144653383+Rohan4053@users.noreply.github.com>
2025-02-07 19:55:05 +0000

    DONE

---
+++ .devcontainer/01_Basics/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log(2.5555>2.55)  //TRUE
+// console.log(2.5555<2.55)  //FALSE
+// console.log("SD"!=98)
+// console.log(98==98)  //True
+// console.log("STRR"=="STRR")  //True
+// console.log("STR"=="str")
+// console.log("STR"=="Str")
+
+// console.log("2">1)  //TRUE
+// console.log("02">"01")   //TRUE
+// console.log("02">"1")    //FALSE
+
+// console.log(null>0)  //FALSE
+// console.log(null==0) //FALSE
+// console.log(null<0)  //FALSE
+// console.log(null>=0)  //TRUE
+// console.log(null<=0)  //TRUE
+
+// console.log(undefined>0)  //FALSE
+// console.log(undefined==0) //FALSE
+// console.log(undefined<0)  //FALSE
+// console.log(undefined>=0)  //FALSE
+// console.log(undefined<=0)  //FALSE
+
+// ===(Strict check also check the data type)
+console.log("2"===2)
